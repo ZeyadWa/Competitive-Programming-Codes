4c55e001b83e3152a8be7cfb79fb28bb600a6ee0
Zeyad <zeyadw526@gmail.com>
2023-10-06 02:32:31 +0300

    MyTemp

---
+++ MyTemp.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> Ordered_Set;
+// *find_by_order, order_of_key
+using ll = long long;
+using ld = long double;
+#define F0R(i, a, b) for (int i = (a); i < (b); ++i)
+#define FOR(i, a) F0R(i, 0, a)
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define mem(x, y) memset( x, y, sizeof x )
+#define SetPre(n, x)  cout << fixed << setprecision(n) << x
+#define Sonic ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define File freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
+#define yes cout << "YES"
+#define no cout << "NO"
+
+void solve()
+{
+
+}
+
+signed main()
+{
+    Sonic
+    int T = 1; // cin >> T;
+    while( T-- )
+    {
+        solve();
+        // I HATE UVA
+        if( T > 0 )
+            cout << endl;
+    }
+    return 0;
+}
